3031ceb7a63a0df27655d0f8b11e0278859b369e
tay0108 <kjkosecki@gmail.com>
2017-11-25 21:52:36 +0100

    css changes

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="stylesheet" type="text/css" href="css/login.css">
     <title>whatuptoo</title>
 </head>
@@ -29,9 +30,9 @@ while ($row = mysqli_fetch_assoc($result)) {
 <div class="login-wrapper">
     <h2 class="login-title">Log in to whatuptoo</h2>
     <form class="login-form" action="./engine/login.php" method="POST">
-        <input type="text" placeholder="Login">
-        <input type="password" placeholder="Password">
-        <button type="submit">Log In</button>
+        <input class="login-field" type="text" placeholder="Login">
+        <input class="login-field" type="password" placeholder="Password">
+        <button class="login-button" type="submit">Log In</button>
     </form>
 </div>
 </body>
